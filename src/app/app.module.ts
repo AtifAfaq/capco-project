9ae63ec33ee4d4a983b0eac4be65285cd2772ede
atif9322011 <atif9322011@gmail.com>
2019-04-20 20:05:48 -0400

    Created project shell

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CapcoTableComponent } from './capco-table/capco-table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CapcoTableComponent
   ],
   imports: [
     BrowserModule,
