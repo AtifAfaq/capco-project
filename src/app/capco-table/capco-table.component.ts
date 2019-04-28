c081e9819791d8543298ec832e3dc25c497ee0aa
atif9322011 <atif9322011@gmail.com>
2019-04-28 15:46:18 -0400

    Fixed selected filter

--- src/app/capco-table/capco-table.component.ts
+++ src/app/capco-table/capco-table.component.ts
@@ -23,7 +23,7 @@ export class CapcoTableComponent implements OnInit {
     20,
     50
   ];
-  constructor(private capcousersservices:CapcousersService) { }
+  constructor(private capcousersservices: CapcousersService) { }
 
   ngOnInit() {
     this.limit = this.resultsLimits[0];
@@ -35,61 +35,61 @@ export class CapcoTableComponent implements OnInit {
     });
   }
 
-onFilterChange(event) {
-  this.filter = event.target.value;
-  this.query = '';
-  this.users = this.capcousersservices.getUsers();
-}
+  onFilterChange(event) {
+    this.filter = event.target.value;
+    this.query = '';
+    this.users = this.capcousersservices.getUsers();
+  }
 
-updateFilterResults() {
-  console.log(this.query);
-  this.users = this.capcousersservices.getUsers();
-}
+  updateFilterResults() {
+    console.log(this.query);
+    this.users = this.capcousersservices.getUsers(this.filter, this.query, this.startIndex);
+  }
 
-onLimitChanged(newLimit) {
-  this.limit = +newLimit;
-  this.startIndex = 0;
-  this.page = 1;
-  this.users = this.capcousersservices.getUsers();
-  this.capcousersservices.getUsers().subscribe(list => {
-    this.pages = list.length / this.limit;
-  });
-}
+  onLimitChanged(newLimit) {
+    this.limit = +newLimit;
+    this.startIndex = 0;
+    this.page = 1;
+    this.users = this.capcousersservices.getUsers();
+    this.capcousersservices.getUsers(this.filter, this.query, this.startIndex).subscribe(list => {
+      this.pages = list.length / this.limit;
+    });
+  }
 
-onPage(pageNumber: number): void {
-  this.page = pageNumber;
-  this.startIndex = (pageNumber * this.limit) - this.limit;
-  this.users = this.capcousersservices.getUsers();
-}
+  onPage(pageNumber: number): void {
+    this.page = pageNumber;
+    this.startIndex = (pageNumber * this.limit) - this.limit;
+    this.users = this.capcousersservices.getUsers();
+  }
 
-onPrev(): void {
-  if(this.page === 1) { return; }
-  this.page = this.page - 1;
-  this.startIndex = this.startIndex - this.limit;
-  this.users = this.capcousersservices.getUsers();
-  // this.atService.getUsers(this.filter, this.query, this.startIndex, (this.startIndex - this.limit))
-  // .subscribe(list => {
-  //   console.log(this, list);
-  // })
-}
-onNext(next: boolean): void {
-  if(this.page === this.pages) { return; }
-  this.page = this.page + 1;
-  this.startIndex = this.startIndex + this.limit;
-  this.users = this.capcousersservices.getUsers();
-  // this.atService.getUsers(this.filter, this.query, this.startIndex, (this.startIndex + this.limit))
-  // .subscribe(list => {
-  //   console.log(this, list);
-  // })
-}
+  onPrev(): void {
+    if (this.page === 1) { return; }
+    this.page = this.page - 1;
+    this.startIndex = this.startIndex - this.limit;
+    this.users = this.capcousersservices.getUsers();
+    // this.atService.getUsers(this.filter, this.query, this.startIndex, (this.startIndex - this.limit))
+    // .subscribe(list => {
+    //   console.log(this, list);
+    // })
+  }
+  onNext(next: boolean): void {
+    if (this.page === this.pages) { return; }
+    this.page = this.page + 1;
+    this.startIndex = this.startIndex + this.limit;
+    this.users = this.capcousersservices.getUsers();
+    // this.atService.getUsers(this.filter, this.query, this.startIndex, (this.startIndex + this.limit))
+    // .subscribe(list => {
+    //   console.log(this, list);
+    // })
+  }
 
-//onSubmitStatus(users) {
- // this.capcousersservices.submitPostRequest(users.id, users.status)
- // .then(resp => {
+  //onSubmitStatus(users) {
+  // this.capcousersservices.submitPostRequest(users.id, users.status)
+  // .then(resp => {
   //  console.log(resp);
-   // users.status = 'Read';
+  // users.status = 'Read';
   //  alert('Successful!');
- // })
+  // })
 }
 
 
